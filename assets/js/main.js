1c2f617c2f7bbecf8444130070ecb9bb186791bf
dev-softheights <to.muhammadarshad1963@gmail.com>
2020-12-16 19:03:58 +0500

    All changes Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,11 @@ var swiper = new Swiper(".swiper-container", {
     slidesPerView: 1,
     spaceBetween: 30,
     loop: true,
+    
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
